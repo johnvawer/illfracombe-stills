830d4d7bb2f53dc45b8123fafa6e4df34afaceb2
Sam Bateman <samlukebateman@gmail.com>
2014-05-21 17:32:58 +0100

    Fixed blinky tooltip

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -14,10 +14,10 @@ $(function() {
 
 			if ((currentPos >= value.offset - 50) && (currentPos <= value.offset + 50))
 			{
-				hideVisibleTooltips(value);				
+				hideVisibleTooltips(value);
 			}
 		});
-	});	
+	});
 
 	$('.cover-arrow').click(function(e){
 		$('html, body').animate({
@@ -26,13 +26,13 @@ $(function() {
 	});
 
 	$('.sidebar-breadcrumb').click(function(e){
-		
+
 		e.preventDefault();
 		var container = $(this).attr('data-target');
-		var timer = getTimer(container);		
+		var timer = getTimer(container);
 
 		$('html,body').animate({
-			scrollTop: $(container).offset().top	
+			scrollTop: $(container).offset().top
 		}, timer);
 	});
 
@@ -42,7 +42,7 @@ $(function() {
 
 		$("[data-nav='scrollElement']").each(function(){
 			elArray.push({
-				element:'#'+$(this).attr('id'), 
+				element:'#'+$(this).attr('id'),
 				offset: $(this).offset().top
 			});
 		});
@@ -50,20 +50,20 @@ $(function() {
 
 	function getTimer(selector){
 
-		var currentScroll = $('body').scrollTop();		
-		var scrollingTo = $(selector).offset().top;		
+		var currentScroll = $('body').scrollTop();
+		var scrollingTo = $(selector).offset().top;
 		var timer = (currentScroll - scrollingTo);
 
 		if(timer < 0)
 		{
 			timer = timer * -1;
 		}
-		
+
 		if(timer > 1000)
 		{
 			timer = 1000;
 		}
-		
+
 		return timer;
 	}
 
@@ -72,27 +72,27 @@ $(function() {
 		var val = value;
 
 		$('.sidebar-breadcrumb').each(function(i, value){
-			
-			if ($(this).attr('data-target') !== val.element)
+			var toHide = $(this).attr('data-target');
+			if (toHide !== val.element)
 			{
-				$("[data-target='"+ val.element +"']").tooltip('hide');	
-			}			
+				$("[data-target='"+ toHide +"']").tooltip('hide');
+			}
 		});
 
-		tooltipToggle(value);	
+		tooltipToggle(value);
 	}
 
 	function tooltipToggle(value) {
-		
+
 		var element = $("[data-target='"+ value.element +"']");
-		
+
 		if (! $('.sidebar-breadcrumb').next().hasClass('tooltip'))
 		{
 			element.tooltip('show');
-			
+
 			setTimeout(function(){
 				element.tooltip('hide');
-			}, 1000);	
-		}			
+			}, 1000);
+		}
 	}
 });
--- wp-content/themes/lh_wordpress_blank_theme/page.php
+++ wp-content/themes/lh_wordpress_blank_theme/page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-        
+
         <div class="sidebar hidden-xs hidden-sm">
 
                 <div class="sidebar-nav clearfix">
@@ -20,18 +20,18 @@
 
 
         <div class="container-full-height jumbotron-image">
-            
+
             <div class="row no-margin-left">
                 <div class="col-md-12">
                     <h1 class="main-title">Ilfracombe Stills</h1>
-                    <h3 class="sub-title text-center">Photographs across North Devon &amp; Exmoor</h3>                    
+                    <h3 class="sub-title text-center">Photographs across North Devon &amp; Exmoor</h3>
                 </div>
             </div>
 
             <div class="cover-arrow"></div>
 
-        </div>            
-        
+        </div>
+
         <div id="gallery-container" class="padding-top-5" data-nav="scrollElement">
             <div class="container">
                 <div class="padding-top-10">
@@ -44,39 +44,39 @@
                         <?php //edit_post_link( __( 'Edit', 'your-theme' ), '<span class="edit-link">', '</span>' ) ?>
                         </div><!-- .entry-content -->
                     </div><!-- #post-<?php the_ID(); ?> -->
-                </div>   
-            </div>         
+                </div>
+            </div>
         </div>
 
         <div class="container-full-height" id="about-container" data-nav="scrollElement">
             <div class="container">
-                <div class="row">
+                <div class="row row-additional-padding">
                     <div class="col-md-12">
                         <h1 class="section-title">About Me</h1>
                     </div>
                     <div class="col-md-12">
                         <p>
-                            My name is Steve and I have lived in Ilfracombe for the past seven years. 
+                            My name is Steve and I have lived in Ilfracombe for the past seven years.
                             Although a Hotelier by trade, I had always had a keen interest in photography.
-                            Unfortunately several years ago I was diagnosed with a debilitating illness "Adult Onset Stills Disease"  (www.stillsrus.co.uk) 
-                            which meant suddenly I was unable to carry on with my work, however as they say "when one ship sails, another comes along" which was quite true, 
-                            and I found myself with more time available to continue with my love of photography. Having been a keen photographer for over 20 years, this has 
+                            Unfortunately several years ago I was diagnosed with a debilitating illness "Adult Onset Stills Disease"  (www.stillsrus.co.uk)
+                            which meant suddenly I was unable to carry on with my work, however as they say "when one ship sails, another comes along" which was quite true,
+                            and I found myself with more time available to continue with my love of photography. Having been a keen photographer for over 20 years, this has
                             ultimately become my passion.
                         </p>
-                        <p>        
+                        <p>
                             I now take my camera with me whenever I go out!!
                         </p>
-                        <p>    
-                            I really enjoy the varied scenery and photographic opportunities North Devon has to offer, and as I am sure you can see from the photographs 
+                        <p>
+                            I really enjoy the varied scenery and photographic opportunities North Devon has to offer, and as I am sure you can see from the photographs
                             in my gallery, the scenery is constantly changing and the colours are spectacular.
                         </p>
-                        <p>    
-                            If you have a particular memory of a favourite view or scene in North Devon and wished you had taken that "special" picture prior to leaving, 
+                        <p>
+                            If you have a particular memory of a favourite view or scene in North Devon and wished you had taken that "special" picture prior to leaving,
                             give me a call or drop me an e-mail. I would be more than willing to discuss visiting the site for you and capturing that photograph you long for...
                         </p>
                     </div>
                 </div>
-            </div>        
+            </div>
         </div>
 
 <?php get_sidebar(); ?>
